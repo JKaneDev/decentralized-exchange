6e893a6fd070b7226bbdad920fe70483c31d2c48
James Kane <james.kane59@outlook.com>
2023-03-12 18:47:42 +0000

    create basic variables for token and test

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.19;
 
 contract Token {
-    string public name = "My Name";
+    string public name = "X Token";
+    string public symbol = 'XTK';
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    // Track Balances
+    mapping(address => uint256) public balanceOf;
+    // Send Tokens
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** 18);
+        balanceOf[msg.sender] = totalSupply;
+    }
 }
