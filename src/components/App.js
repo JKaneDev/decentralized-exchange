32766daae9fb8315690d61b005d2510e44a5daf3
James Kane <james.kane59@outlook.com>
2023-03-19 13:28:12 +0000

    render content if contracts loaded

--- src/components/App.js
+++ src/components/App.js
@@ -8,8 +8,9 @@ import {
 	loadToken,
 	loadExchange,
 } from '../store/interactions';
-import { accountSelector } from '../store/selectors';
 import NavBar from './NavBar';
+import Content from './Content';
+import { contractLoadedSelector } from '../store/selectors';
 
 function App(props) {
 	useEffect(() => {
@@ -19,7 +20,7 @@ function App(props) {
 	const loadBlockchainData = async (dispatch) => {
 		const web3 = await loadWeb3(dispatch);
 		const networkId = await web3.eth.net.getId();
-		const accounts = await loadAccount(web3, dispatch);
+		await loadAccount(web3, dispatch);
 		const token = await loadToken(web3, networkId, dispatch);
 		loadExchange(web3, networkId, dispatch);
 	};
@@ -27,95 +28,14 @@ function App(props) {
 	return (
 		<div>
 			<NavBar />
-			<div className='content'>
-				<div className='vertical-split'>
-					<div className='card bg-dark text-white'>
-						<div className='card-header'>Card Title</div>
-						<div className='card-body'>
-							<p className='card-text'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-							<a href='/#' className='card-link'>
-								Card link
-							</a>
-						</div>
-					</div>
-					<div className='card bg-dark text-white'>
-						<div className='card-header'>Card Title</div>
-						<div className='card-body'>
-							<p className='card-text'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-							<a href='/#' className='card-link'>
-								Card link
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className='vertical'>
-					<div className='card bg-dark text-white'>
-						<div className='card-header'>Card Title</div>
-						<div className='card-body'>
-							<p className='card-text'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-							<a href='/#' className='card-link'>
-								Card link
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className='vertical-split'>
-					<div className='card bg-dark text-white'>
-						<div className='card-header'>Card Title</div>
-						<div className='card-body'>
-							<p className='card-text'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-							<a href='/#' className='card-link'>
-								Card link
-							</a>
-						</div>
-					</div>
-					<div className='card bg-dark text-white'>
-						<div className='card-header'>Card Title</div>
-						<div className='card-body'>
-							<p className='card-text'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-							<a href='/#' className='card-link'>
-								Card link
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className='vertical'>
-					<div className='card bg-dark text-white'>
-						<div className='card-header'>Card Title</div>
-						<div className='card-body'>
-							<p className='card-text'>
-								Some quick example text to build on the card title and make up
-								the bulk of the card's content.
-							</p>
-							<a href='/#' className='card-link'>
-								Card link
-							</a>
-						</div>
-					</div>
-				</div>
-			</div>
+			{props.contractsLoaded ? <Content /> : <div className='content'></div>}
 		</div>
 	);
 }
 
 function mapStateToProps(state) {
 	return {
-		// TODO: fill me in...
+		contractsLoaded: contractLoadedSelector(state),
 	};
 }
 
--- src/store/selectors.js
+++ src/store/selectors.js
@@ -2,6 +2,21 @@ import { get } from 'lodash';
 import { createSelector } from 'reselect';
 
 const account = (state) => {
-	return state.web3.account;
+	return get(state, 'web3.account');
 };
 export const accountSelector = createSelector(account, (a) => a);
+
+const tokenLoaded = (state) => (state.token ? true : false);
+export const tokenLoadedSelector = createSelector(tokenLoaded, (tl) => tl);
+
+const exchangeLoaded = (state) => (state.exchange ? true : false);
+export const exchangeLoadedSelector = createSelector(
+	exchangeLoaded,
+	(el) => el
+);
+
+export const contractLoadedSelector = createSelector(
+	tokenLoaded,
+	exchangeLoaded,
+	(tl, el) => tl && el
+);
