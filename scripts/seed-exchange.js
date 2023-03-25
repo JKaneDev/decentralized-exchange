803cab37a40106f27fae6b89e59ed79c91cffd1d
James Kane <james.kane59@outlook.com>
2023-03-25 11:27:02 +0000

    prep deployment

--- scripts/seed-exchange.js
+++ scripts/seed-exchange.js
@@ -7,7 +7,7 @@ const wait = helpers.wait;
 const Token = artifacts.require('Token');
 const Exchange = artifacts.require('Exchange');
 
-module.exports = async function (callback) {
+module.exports = async function () {
 	try {
 		// Fetch accounts
 		const accounts = await web3.eth.getAccounts();
@@ -31,8 +31,11 @@ module.exports = async function (callback) {
 		const user1 = accounts[0];
 		const user2 = accounts[1];
 
+		const balance = await web3.eth.getBalance(accounts[0]);
+		console.log('User 1 Balance: ', balance);
+
 		// User1 deposits ETH
-		amount = 1;
+		amount = 0.1;
 		await exchange.depositEther({ from: user1, value: ether(amount) });
 		console.log(`Deposited ${amount} ETH from ${user1}`);
 
@@ -57,13 +60,9 @@ module.exports = async function (callback) {
 
 		let result;
 		let orderId;
-		result = await exchange.makeOrder(
-			token.address,
-			tokens(100),
-			ETH_ADDRESS,
-			ether(0.1),
-			{ from: user1 }
-		);
+		result = await exchange.makeOrder(token.address, tokens(100), ETH_ADDRESS, ether(0.1), {
+			from: user1,
+		});
 
 		// User1 cancels order
 		// console.log(result);
@@ -78,13 +77,9 @@ module.exports = async function (callback) {
 		 */
 
 		// User 1 makes order
-		result = await exchange.makeOrder(
-			token.address,
-			tokens(100),
-			ETH_ADDRESS,
-			ether(0.1),
-			{ from: user1 }
-		);
+		result = await exchange.makeOrder(token.address, tokens(100), ETH_ADDRESS, ether(0.1), {
+			from: user1,
+		});
 		console.log(`Made order from ${user1}`);
 
 		// User 2 fills order
@@ -96,13 +91,9 @@ module.exports = async function (callback) {
 		await wait(1);
 
 		// User 1 makes another order
-		result = await exchange.makeOrder(
-			token.address,
-			tokens(50),
-			ETH_ADDRESS,
-			ether(0.01),
-			{ from: user1 }
-		);
+		result = await exchange.makeOrder(token.address, tokens(50), ETH_ADDRESS, ether(0.01), {
+			from: user1,
+		});
 		console.log(`Made order from ${user1}`);
 
 		// User 2 fills another order
@@ -114,13 +105,9 @@ module.exports = async function (callback) {
 		await wait(1);
 
 		// User 1 makes final order
-		result = await exchange.makeOrder(
-			token.address,
-			tokens(200),
-			ETH_ADDRESS,
-			ether(0.15),
-			{ from: user1 }
-		);
+		result = await exchange.makeOrder(token.address, tokens(200), ETH_ADDRESS, ether(0.15), {
+			from: user1,
+		});
 		console.log(`Made order from ${user1}`);
 
 		// User 2 fills final order
@@ -139,13 +126,9 @@ module.exports = async function (callback) {
 
 		// User 1 makes 10 orders
 		for (let i = 1; i <= 10; i++) {
-			result = await exchange.makeOrder(
-				token.address,
-				tokens(10 * i),
-				ETH_ADDRESS,
-				ether(0.01),
-				{ from: user1 }
-			);
+			result = await exchange.makeOrder(token.address, tokens(10 * i), ETH_ADDRESS, ether(0.01), {
+				from: user1,
+			});
 			console.log(`Made order from ${user1}`);
 			// Wait 1 second
 			await wait(1);
@@ -153,13 +136,9 @@ module.exports = async function (callback) {
 
 		// User 2 makes 10 orders
 		for (let i = 1; i <= 10; i++) {
-			result = await exchange.makeOrder(
-				ETH_ADDRESS,
-				ether(0.01),
-				token.address,
-				tokens(10 * i),
-				{ from: user2 }
-			);
+			result = await exchange.makeOrder(ETH_ADDRESS, ether(0.01), token.address, tokens(10 * i), {
+				from: user2,
+			});
 			console.log(`Made order from ${user2}`);
 			// Wait 1 second
 			await wait(1);
@@ -168,5 +147,5 @@ module.exports = async function (callback) {
 		console.log(err);
 	}
 
-	callback();
+	return;
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,28 @@
 require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const privateKeys = process.env.PRIVATE_KEYS || '';
+const API_KEY = process.env.INFURA_API_KEY || '';
+
 module.exports = {
 	networks: {
 		development: {
 			host: '127.0.0.1',
 			port: 7545,
 			network_id: '*',
+		},
+		sepolia: {
+			provider: function () {
+				return new HDWalletProvider(
+					// Private Key
+					privateKeys.split(','),
+					// URL to an Ethereum Node
+					`https://sepolia.infura.io/v3/${API_KEY}`,
+				);
+			},
+			gas: 6000000,
+			gasPrice: 20000000000,
+			network_id: 11155111,
 		},
 	},
 	contracts_directory: './src/contracts/',
